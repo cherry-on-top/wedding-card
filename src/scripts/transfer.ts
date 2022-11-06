946300c38dce9a56da9d765fa29baed8df5fff27
Penguin <say8425@gmail.com>
2022-11-06 19:31:01 +0900

    ✨ 계좌정보 복사하기에 toast 달아줌 * 하면서 ts 로 코드 분리

---
+++ src/scripts/transfer.ts
@@ -0,0 +1,38 @@
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+  toast: true,
+  showConfirmButton: false,
+  timerProgressBar: false,
+  timer: 3000,
+  position: "bottom",
+});
+for (const element of document.querySelectorAll(".account-info")) {
+  element.addEventListener("click", (event) => {
+    const account = (event.target as HTMLElement).closest(".account");
+    const accountNumber =
+      (account?.querySelector(".account-number") as HTMLParagraphElement)
+        ?.innerText ?? "";
+    const accountName =
+      (account?.querySelector(".account-name") as HTMLParagraphElement)
+        ?.innerText ?? "";
+    const accountText = `${accountName} ${accountNumber}`.trim();
+
+    if (!accountText) {
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(accountText)
+      .then(async () => {
+        await Toast.fire({
+          title: "계좌정보가 복사되었습니다.",
+        });
+      })
+      .catch(async () => {
+        await Toast.fire({
+          title: "계좌정보 복사에 실패했습니다.",
+        });
+      });
+  });
+}
